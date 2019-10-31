ad1d6b7798e8d7b25ebcd0d07d0e6e44000e1079
Alexis Hernandez <44562141+Alexis260698@users.noreply.github.com>
2019-10-31 01:26:36 -0600

    BDD
    base de datos, y daos

---
+++ app/src/main/java/com/example/proyecto/DaoNotasTareas.java
@@ -0,0 +1,22 @@
+package com.example.proyecto;
+
+public class DaoNotasTareas {
+
+    public void insertarNota(){
+
+    }
+
+    public void Actualizar(){
+
+    }
+
+    public void Eliminar(){
+
+    }
+
+    public void Buscar(){
+
+    }
+
+
+}
