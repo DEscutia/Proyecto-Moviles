44bd69e94e15d9f28c2063e82feb3ad705b6c310
Alexis Hernandez <44562141+Alexis260698@users.noreply.github.com>
2019-11-18 03:02:18 -0600

    Fecha; Hora y agregar nota
    agregar notas a medias

--- app/src/main/java/com/example/proyecto/AgregarNota.java
+++ app/src/main/java/com/example/proyecto/AgregarNota.java
@@ -3,7 +3,10 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,15 +14,19 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
 
 import java.util.Calendar;
 
 public class AgregarNota extends AppCompatActivity {
 
-    Button btnSeleccionarFecha,btnGuardar;
-    TextView txtTitulo, txtDescripcion, txtRecordatorio, tvFecha;
-    EditText etTitulo, etDescripcion, etHora;
-    RadioButton rbtnNota,rbtnTarea;
+    Button btnSeleccionarFecha, btnGuardar, btnHora;
+    TextView txtTitulo, txtDescripcion, txtRecordatorio, tvFecha, tvHora;
+    EditText etTitulo, etDescripcion;
+    RadioButton rbtnNota, rbtnTarea;
+    BDD conn;
 
     private static final String CERO = "0";
     private static final String BARRA = "/";
@@ -27,41 +34,44 @@ public class AgregarNota extends AppCompatActivity {
     final int mes = calendar.get(Calendar.MONTH);
     final int dia = calendar.get(Calendar.DAY_OF_MONTH);
     final int año = calendar.get(Calendar.YEAR);
-    String fecha="";
+    String fecha = "";
+    private int horas, minutos;
 
+    private int Tarea=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_nota);
 
-        btnSeleccionarFecha=(Button)findViewById(R.id.btnSeleccionarFecha);
-        btnGuardar=(Button)findViewById(R.id.btnGuardar);
-        etTitulo=(EditText)findViewById(R.id.etTitulo);
-        etDescripcion=(EditText)findViewById(R.id.etDescripcion);
-        etHora=(EditText)findViewById(R.id.etHora);
-        txtTitulo=(TextView)findViewById(R.id.txtTitulo);
-        txtDescripcion=(TextView)findViewById(R.id.txtDescripcion);
-        txtRecordatorio=(TextView)findViewById(R.id.txtRecordatorio);
-        tvFecha=(TextView)findViewById(R.id.tvFecha);
-        rbtnNota=(RadioButton)findViewById(R.id.rbtnNota);
-        rbtnTarea=(RadioButton)findViewById(R.id.rbtnTarea);
+        btnSeleccionarFecha = (Button) findViewById(R.id.btnSeleccionarFecha);
+        btnGuardar = (Button) findViewById(R.id.btnGuardar);
+        etTitulo = (EditText) findViewById(R.id.etTitulo);
+        etDescripcion = (EditText) findViewById(R.id.etDescripcion);
+        tvHora = (TextView) findViewById(R.id.tvHora);
+        txtTitulo = (TextView) findViewById(R.id.txtTitulo);
+        txtDescripcion = (TextView) findViewById(R.id.txtDescripcion);
+        txtRecordatorio = (TextView) findViewById(R.id.txtRecordatorio);
+        tvFecha = (TextView) findViewById(R.id.tvFecha);
+        rbtnNota = (RadioButton) findViewById(R.id.rbtnNota);
+        rbtnTarea = (RadioButton) findViewById(R.id.rbtnTarea);
+        btnHora = (Button) findViewById(R.id.btnHora);
 
-
-
+        conn=new BDD(getApplicationContext(),"PFinal",null,1);
     }
-    public void obtenerFecha(View view){
+
+    public void obtenerFecha(View view) {
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 //Esta variable lo que realiza es aumentar en uno el mes ya que comienza desde 0 = enero
                 final int mesActual = month + 1;
                 //Formateo el día obtenido: antepone el 0 si son menores de 10
-                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                String diaFormateado = (dayOfMonth < 10) ? CERO + String.valueOf(dayOfMonth) : String.valueOf(dayOfMonth);
                 //Formateo el mes obtenido: antepone el 0 si son menores de 10
-                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+                String mesFormateado = (mesActual < 10) ? CERO + String.valueOf(mesActual) : String.valueOf(mesActual);
                 //Muestro la fecha con el formato deseado
-                fecha=year + BARRA + mesFormateado + BARRA + diaFormateado;
+                fecha = year + BARRA + mesFormateado + BARRA + diaFormateado;
                 tvFecha.setText(year + BARRA + mesFormateado + BARRA + diaFormateado);
             }
         }, año, mes, dia);
@@ -69,10 +79,62 @@ public class AgregarNota extends AppCompatActivity {
     }
 
 
+    public void obtenerHora(View view) {
+        final Calendar c = Calendar.getInstance();
+        horas = c.get(Calendar.HOUR_OF_DAY);
+        minutos = c.get(Calendar.MINUTE);
 
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                tvHora.setText(hourOfDay + ":" + minute);
+            }
+        }, horas, minutos, false);
+        timePickerDialog.show();
+    }
+
+    public void Nota(View view) {
+        txtRecordatorio.setVisibility(View.GONE);
+        tvFecha.setVisibility(View.GONE);
+        btnSeleccionarFecha.setVisibility(View.GONE);
+        tvHora.setVisibility(View.GONE);
+        btnHora.setVisibility(View.GONE);
+        Tarea=0;
+    }
+
+    public void Tarea(View view) {
+        txtRecordatorio.setVisibility(View.VISIBLE);
+        tvFecha.setVisibility(View.VISIBLE);
+        btnSeleccionarFecha.setVisibility(View.VISIBLE);
+        tvHora.setVisibility(View.VISIBLE);
+        btnHora.setVisibility(View.VISIBLE);
+        Tarea=1;
+    }
 
     public void Guardar(View view){
-        Intent myintent= new Intent(AgregarNota.this,MainActivity.class);
+        Intent myintent = new Intent(AgregarNota.this, MainActivity.class);
         startActivity(myintent);
     }
+
+
+    public void AgregarNota(){
+        SQLiteDatabase db=conn.getWritableDatabase();
+        ContentValues values= new ContentValues();
+
+        if(Tarea==1){
+            values.put(CrearTabla.titulo,etTitulo.getText().toString());
+            values.put(CrearTabla.Descripcion,txtDescripcion.getText().toString());
+            //values.put(CrearTabla.fechaCreado,().toString());   obtener fecha de ejecucion
+            values.put(CrearTabla.fechaLimite,tvFecha.getText().toString());
+            values.put(CrearTabla.HoraLimite,tvHora.getText().toString());
+            //enviar valor en el campo de cumplida
+            values.put(CrearTabla.Tarea,Tarea);
+        }else{
+            values.put(CrearTabla.titulo,etTitulo.getText().toString());
+            values.put(CrearTabla.Descripcion,txtDescripcion.getText().toString());
+            values.put(CrearTabla.Tarea,Tarea);
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/proyecto/BDD.java
+++ app/src/main/java/com/example/proyecto/BDD.java
@@ -11,7 +11,7 @@ public class BDD extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "PFinal";
 
-    public BDD(Context context) {
+    public BDD(Context applicationContext, String pFinal, Context context, int i) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
