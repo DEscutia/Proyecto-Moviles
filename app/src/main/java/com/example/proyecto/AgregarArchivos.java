d8ad94e3086baaa2aa0b8c860898b8efa63efebd
DEscutia <44597771+DEscutia@users.noreply.github.com>
2019-11-19 02:42:00 -0600

    Add multimedia

--- app/src/main/java/com/example/proyecto/AgregarArchivos.java
+++ app/src/main/java/com/example/proyecto/AgregarArchivos.java
@@ -1,22 +1,229 @@
 package com.example.proyecto;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AgregarArchivos extends AppCompatActivity {
+
+    ImageView ivFoto;
+    VideoView ivVideo;
+    Button btnTomarFoto, btnSeleccionar;
+    Uri imagenUri;
+    int TOMAR_FOTO = 100;
+    int TOMAR_VIDEO = 300;
+    int SELEC_IMAGEN = 200;
+
+    String CARPETA_RAIZ = "MisFotos/";
+    String CARPETA_IMAGENES = "imagenes";
+    String RUTA_IMAGEN = CARPETA_RAIZ + CARPETA_IMAGENES;
+    String path;
+
+    private MediaRecorder grabacion;
+    private String archivoSalida = null;
+    private Button btn_recorder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_archivos);
+
+        ivFoto =findViewById(R.id.imgFoto);
+        btnTomarFoto = findViewById(R.id.btnCamara);
+        btnSeleccionar = findViewById(R.id.btnBuscarArchivo);
+        btn_recorder = findViewById(R.id.btnAudio);
+
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(AgregarArchivos.this,
+                    new String[]{Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.RECORD_AUDIO}, 0);
+        }
+
+        btnTomarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tomarFoto();
+            }
+        });
+
+        btnSeleccionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seleccionarArchivo();
+            }
+
+
+        });
+
+        btn_recorder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                grabar(v);
+            }
+        });
+
     }
 
+    public void seleccionarArchivo() {
+        Intent galeria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(galeria, SELEC_IMAGEN);
+    }
+
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private final static String MY_PROVIDER = BuildConfig.APPLICATION_ID + ".providers.FileProvider";
+
+    private void tomarFoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        MY_PROVIDER,
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, TOMAR_FOTO);
+            }
+        }
+    }
+
+    public void tomarVideo(View view) {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, TOMAR_VIDEO);
+        }
+    }
+
+
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode == RESULT_OK && requestCode == SELEC_IMAGEN) {
+            imagenUri = data.getData();
+            ivFoto.setImageURI(imagenUri);
+        } else if(resultCode == RESULT_OK && requestCode == TOMAR_FOTO) {
+            MediaScannerConnection.scanFile(AgregarArchivos.this, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                @Override
+                public void onScanCompleted(String s, Uri uri) {
+
+                }
+            });
+            Log.i("MULTIMEDIA",currentPhotoPath);
+            Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+            ivFoto.setImageBitmap(bitmap);
+
+        }else if (requestCode == TOMAR_VIDEO && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            ivVideo.setVideoURI(videoUri);
+        }
+
+    }
+
+
+    //Grabar Audio
+    public void grabar(View view){
+        if (grabacion == null){
+            archivoSalida = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.mp3";
+            grabacion = new MediaRecorder();
+            grabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
+            grabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            grabacion.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            grabacion.setOutputFile(archivoSalida);
+
+            try{
+                grabacion.prepare();
+                grabacion.start();
+            }catch (IOException e){
+
+            }
+
+            btn_recorder.setText("Grabando...");
+            Toast.makeText(getApplicationContext(), "Grabando...",Toast.LENGTH_SHORT).show();
+        }else if(grabacion!=null){
+                grabacion.stop();
+                grabacion.release();
+                grabacion = null;
+
+                btn_recorder.setText("Grabar");
+            Toast.makeText(getApplicationContext(), "Grabación finalizada",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void reproducir (View view){
+        MediaPlayer mediaPlayer = new MediaPlayer();
+            try{
+                mediaPlayer.setDataSource(archivoSalida);
+                mediaPlayer.prepare();
+
+            }catch (IOException e){
+
+            }
+
+            mediaPlayer.start();
+        Toast.makeText(getApplicationContext(), "Reproduciendo audio",Toast.LENGTH_SHORT).show();
+    }
 
     public void Agregar(View view){
         Intent myintent= new Intent(AgregarArchivos.this,DetalleNT.class);
         startActivity(myintent);
     }
+
+
+
 }
